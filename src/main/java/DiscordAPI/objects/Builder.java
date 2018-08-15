c35c49da2a5c1c3d5dfc3867785a708770028f69
ideaeclipse <36806775+ideaeclipse@users.noreply.github.com>
2018-08-14 22:50:57 -0400

    JavaDoc comments

--- src/main/java/DiscordAPI/objects/Builder.java
+++ src/main/java/DiscordAPI/objects/Builder.java
@@ -6,11 +6,21 @@ import org.json.simple.JSONObject;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+/**
+ * This class is used for Building JSONObject {@link JSONObject}
+ * Based on the payload structure Discord's Websocket handles
+ *
+ * @author Myles
+ */
 public class Builder {
     static class CreateDmChannel {
         Long recipient_id;
     }
 
+    /**
+     * Used for creating an identity payload
+     * This is used in {@link DiscordBot#buildIdentity()}
+     */
     static class Identity {
         String token;
         //Properties Class
@@ -21,6 +31,10 @@ public class Builder {
         JSONObject presence;
         ArrayList<Integer> shards = new Shard().getInts();
 
+        /**
+         * Used for creating an Shard object used in the identity Payload
+         * This is used in {@link DiscordBot#buildIdentity()}
+         */
         static class Shard {
             ArrayList<Integer> ints = new ArrayList<>();
 
@@ -33,20 +47,29 @@ public class Builder {
                 return ints;
             }
         }
-
+        /**
+         * Used for creating an Presnece object used in the identity Payload
+         * This is used in {@link DiscordBot#buildIdentity()}
+         */
         static class Presence {
             //Game
             JSONObject game;
             String status = "online";
             Long since = null;
             Boolean afk = false;
-
+            /**
+             * Used for creating an Game object used in the identity Payload
+             * This is used in {@link DiscordBot#buildIdentity()}
+             */
             static class Game {
                 String name = "cm help for commands";
                 Integer type = Payloads.GameTypes.Playing.ordinal();
             }
         }
-
+        /**
+         * Used for creating an Properties object used in the identity Payload
+         * This is used in {@link DiscordBot#buildIdentity()}
+         */
         static class Properties {
             String $os = System.getProperty("os.name");
             String $browser = "D4J";
@@ -54,6 +77,13 @@ public class Builder {
         }
     }
 
+    /**
+     * Method is used to create a payload to send over the Discord TextWss {@link DiscordAPI.webSocket.Wss}
+     *
+     * @param code OpCode Value {@link OpCodes}
+     * @param data Json Object to file 'd' param use {@link Builder#buildData(Object)}
+     * @return Complete payload ready to send over the websocket
+     */
     public static JSONObject buildPayload(OpCodes code, Object data) {
         JSONObject object = new JSONObject();
         object.put("op", code.ordinal());
@@ -61,6 +91,13 @@ public class Builder {
         return object;
     }
 
+    /**
+     * This method is used for the Object param in buildPayload {@link Builder#buildPayload(OpCodes, Object)}
+     *
+     * @param generic is an instance of an Object that in the Builder Class {@link Builder}
+     * @param <T> is the generic Type
+     * @return Returns a Json Data used for the 'd' value in the Wss payload
+     */
     static <T> JSONObject buildData(T generic) {
         JSONObject data = new JSONObject();
         for (Field f : generic.getClass().getDeclaredFields()) {
