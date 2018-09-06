79f14998b01e24a717fa193906be0185f3b22cd0
ideaeclipse <36806775+ideaeclipse@users.noreply.github.com>
2018-09-06 09:34:54 -0700

    async Dispatcher

--- src/main/java/DiscordAPI/listener/genericListener/IDispatcher.java
+++ src/main/java/DiscordAPI/listener/genericListener/IDispatcher.java
@@ -1,5 +1,7 @@
 package DiscordAPI.listener.genericListener;
 
+import DiscordAPI.utils.Async;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +12,20 @@ public class IDispatcher {
         listeners = new ArrayList<>();
     }
 
-    public <K, T extends K> void addListener(IListener<K,T> listener) {
+    public <K, T extends K> void addListener(IListener<K, T> listener) {
         listeners.add(listener);
     }
 
-    public <K,T extends K> void notify(T listenerType) {
-        for (IListener ev : listeners) {
-            try {
-                ev.handle(listenerType);
-            } catch (ClassCastException ignored) {
+    public <K, T extends K> void notify(T listenerType) {
+        Async.queue(() -> {
+            for (IListener ev : listeners) {
+                try {
+                    ev.handle(listenerType);
+                } catch (ClassCastException ignored) {
 
+                }
             }
-        }
+            return null;
+        }, "Listener-" + listenerType.getClass().getSimpleName());
     }
 }
